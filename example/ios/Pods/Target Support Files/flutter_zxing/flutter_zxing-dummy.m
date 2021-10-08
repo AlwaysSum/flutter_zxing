d4e8d847e79a502f5679a2fe2a15e46da6997d92
maguangcan <maguangcan@bloks.com>
2021-10-08 17:51:50 +0800

    first commit

---
+++ example/ios/Pods/Target Support Files/flutter_zxing/flutter_zxing-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_flutter_zxing : NSObject
+@end
+@implementation PodsDummy_flutter_zxing
+@end
---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,35 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<camera/CameraPlugin.h>)
+#import <camera/CameraPlugin.h>
+#else
+@import camera;
+#endif
+
+#if __has_include(<flutter_zxing/FlutterZxingPlugin.h>)
+#import <flutter_zxing/FlutterZxingPlugin.h>
+#else
+@import flutter_zxing;
+#endif
+
+#if __has_include(<fluttertoast/FluttertoastPlugin.h>)
+#import <fluttertoast/FluttertoastPlugin.h>
+#else
+@import fluttertoast;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [CameraPlugin registerWithRegistrar:[registry registrarForPlugin:@"CameraPlugin"]];
+  [FlutterZxingPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterZxingPlugin"]];
+  [FluttertoastPlugin registerWithRegistrar:[registry registrarForPlugin:@"FluttertoastPlugin"]];
+}
+
+@end
